64e3ed02c7680b5a0c4c914b68b2202d70f2b6b4
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2021-05-11 09:06:51 -0300

    First commit

---
+++ src/main/java/br/com/locadoracarros/carrental/CarRentalApplication.java
@@ -0,0 +1,14 @@
+package br.com.locadoracarros.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalApplication.class, args);
+	}
+
+	// Criação da aplicação
+}
