bce9ef55db4c0966f734c4d95645f206bd4b0d7d
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2021-06-01 09:26:17 -0300

    Added GetMapping to get all categories

--- src/main/java/br/com/locadoracarros/carrental/controller/CategoryController.java
+++ src/main/java/br/com/locadoracarros/carrental/controller/CategoryController.java
@@ -1,10 +1,18 @@
 package br.com.locadoracarros.carrental.controller;
 
+import br.com.locadoracarros.carrental.entities.Car;
+import br.com.locadoracarros.carrental.entities.Category;
 import br.com.locadoracarros.carrental.service.CategoryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +28,39 @@ public class CategoryController {
 	//autowired for CategoryService
 	@Autowired
 	CategoryService categoryService;
+
+
+	//first GetMapping
+	@ApiOperation(value = "Lista todas as categorias", notes = "Lista todas as categorias",
+			response = Car.class, responseContainer = "Page")
+	@ApiResponses(value = {
+			@ApiResponse(code = 204, message = "Categorias listadas com sucesso")
+	})
+	@GetMapping
+	public Page<Category> getAllCategories(
+			@RequestParam(
+					value = "page",
+					required = false,
+					defaultValue = "0") int page,
+			@RequestParam(
+					value = "size",
+					required = false,
+					defaultValue = "15") int size,
+			@RequestParam(
+					defaultValue = "desc",
+					value = "sort",
+					required = false) String sort,
+			@RequestParam(
+					defaultValue = "",
+					value = "q",
+					required = false) String q,
+			@RequestParam(
+					defaultValue = "model",
+					value = "attribute",
+					required = false) String attribute)
+	{
+
+		return this.categoryService.getAll(page, size, sort, q, attribute);
+	}
 
 }
