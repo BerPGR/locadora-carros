464625217a075f328dc80d706ccf521600ab8e45
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2021-06-01 09:36:26 -0300

    Added GetMapping by ID

--- src/main/java/br/com/locadoracarros/carrental/controller/CategoryController.java
+++ src/main/java/br/com/locadoracarros/carrental/controller/CategoryController.java
@@ -10,10 +10,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
@@ -63,4 +64,32 @@ public class CategoryController {
 		return this.categoryService.getAll(page, size, sort, q, attribute);
 	}
 
+
+	//Operation GetMapping for one category based on ID
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@ApiOperation(value = "Obtém uma categoria", notes = "Obtém uma categoria")
+	@ApiResponses(value = {
+			@ApiResponse(code = 204, message = "Existe uma categoria")
+	})
+	@GetMapping("/{id}")
+	public ResponseEntity<Category> getCategory(@PathVariable("id") int id){
+		try {
+			Optional<Category> optionalCategory = this.categoryService.getCategory(id);
+
+			ResponseEntity response;
+			if (optionalCategory.isPresent()) {
+
+				response = ResponseEntity.ok(optionalCategory.get());
+
+			} else {
+
+				response = ResponseEntity.noContent().build();
+
+			}
+			return response;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.unprocessableEntity().build();
+		}
+	}
 }
