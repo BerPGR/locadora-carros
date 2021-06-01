ceccfecf36ffe6b6f837e1c8f032e888d99fda51
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2021-06-01 09:22:03 -0300

    Add endPoint, logger and categoryService to CategoryController

--- src/main/java/br/com/locadoracarros/carrental/controller/CategoryController.java
+++ src/main/java/br/com/locadoracarros/carrental/controller/CategoryController.java
@@ -1,9 +1,24 @@
 package br.com.locadoracarros.carrental.controller;
 
+import br.com.locadoracarros.carrental.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
+
+	//category endpoint
+	private final String endPoint = "/category";
+
+	//logger for category
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	//autowired for CategoryService
+	@Autowired
+	CategoryService categoryService;
+
 }
